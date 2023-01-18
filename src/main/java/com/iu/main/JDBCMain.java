641584f01c46a85eecff8076688702b8a9a79851
Daeki <kimdaeki@gmail.com>
2023-01-18 17:50:44 +0900

    add

--- src/main/java/com/iu/main/JDBCMain.java
+++ src/main/java/com/iu/main/JDBCMain.java
@@ -1,16 +1,21 @@
 package com.iu.main;
 
+import com.iu.main.departments.DepartmentController;
+import com.iu.main.departments.DepartmentDAO;
+import com.iu.main.locations.LocationController;
+import com.iu.main.locations.LocationDAO;
 import com.iu.main.util.DBConnection;
 
 public class JDBCMain {
 
 	public static void main(String[] args) {
 		System.out.println("start");
-		DBConnection con = new DBConnection();
-		
-		//참조변수명.멤버들
+		DepartmentController dc = new DepartmentController();
+		LocationController lc = new LocationController();
+	
 		try {
-			con.getConnection();
+//			lc.start();
+			dc.start();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/main/java/com/iu/main/util/DBConnection.java
+++ src/main/java/com/iu/main/util/DBConnection.java
@@ -7,34 +7,33 @@ import java.sql.ResultSet;
 
 public class DBConnection {
 	
-	public void getConnection() throws Exception {
+	public static Connection getConnection() throws Exception {
+		//1. 접속 정보 준비
+		
 		//1. id
 		String username="hr";
 		//2. pw
 		String password="hr";
 		//3. url
+		//String url="jdbc:oracle:thin:@ip:port/ServiceName";
+		//String url="jdbc:oracle:thin:@ip:port:SID";		
 		String url="jdbc:oracle:thin:@localhost:1521/XEPDB1";
 		//4. driver
 		String driver="oracle.jdbc.driver.OracleDriver";
 		
+		
 		Class.forName(driver);
 		
+
 		Connection connection = DriverManager.getConnection(url, username, password);
-		
-		String sql="SELECT * FROM REGIONS WHERE REGION_ID=2";
-		
-		PreparedStatement st = connection.prepareStatement(sql);
-		
-		ResultSet rs = st.executeQuery();
-		
-		while(rs.next()) {
-			System.out.println(rs.getInt("REGION_ID"));
-			System.out.println(rs.getString("REGION_NAME"));
-		}
-		
-		
-		
-		
+		return connection;
+
+	}
+	
+	public static void disConnect(ResultSet rs, PreparedStatement st, Connection connection) throws Exception {
+		rs.close();
+		st.close();
+		connection.close();
 	}
 
 }
